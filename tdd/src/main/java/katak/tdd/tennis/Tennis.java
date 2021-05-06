843724bf0db8084a97dad8d6f6d570895d576339
hharajl1 <hharajl1@ford.com>
2021-05-06 16:52:10 -0400

    adding tests for player1 vs player2 scoring

--- tdd/src/main/java/katak/tdd/tennis/Tennis.java
+++ tdd/src/main/java/katak/tdd/tennis/Tennis.java
@@ -1,4 +1,22 @@
 package katak.tdd.tennis;
 
 public class Tennis {
+    int player1Score = 0;
+    int player2Score = 0;
+
+    public int getPlayer1Score() {
+        return player1Score;
+    }
+
+    public int getPlayer2Score() {
+        return player2Score;
+    }
+
+    public void addToPlayer1Score() {
+        player1Score++;
+    }
+
+    public void addToPlayer2Score() {
+        player2Score++;
+    }
 }
--- tdd/src/test/java/kata/tdd/tennis/TennisTest.java
+++ tdd/src/test/java/kata/tdd/tennis/TennisTest.java
@@ -1,4 +1,44 @@
 package kata.tdd.tennis;
 
+import katak.tdd.tennis.Tennis;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TennisTest {
+
+
+    @Test
+    void newGameReturns0ForPlayersScores() {
+        //given
+        Tennis tennis = new Tennis();
+        //when
+
+        //then
+        assertEquals(tennis.getPlayer1Score(), 0);
+        assertEquals(tennis.getPlayer2Score(), 0);
+    }
+
+    @Test
+    void adds1ToPlayerOnesScore() {
+        //given
+        Tennis tennis = new Tennis();
+
+        //when
+        tennis.addToPlayer1Score();
+        //then
+        assertEquals(tennis.getPlayer1Score(), 1);
+    }
+
+    @Test
+    void adds1ToPlayerTwosScore() {
+        //given
+        Tennis tennis = new Tennis();
+
+        //when
+        tennis.addToPlayer2Score();
+        //then
+        assertEquals(tennis.getPlayer2Score(), 1);
+
+    }
 }
