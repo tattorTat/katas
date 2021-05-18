ddbc51d2191d85ba94b0728b86089c9797add32a
Xing Zhou <xzhou110@ford.com>
2021-05-18 16:30:06 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- duplication/java-version/src/main/java/duplicates/Lesson1Straight.java
+++ duplication/java-version/src/main/java/duplicates/Lesson1Straight.java
@@ -2,13 +2,12 @@ package duplicates;
 
 public class Lesson1Straight extends Song {
     public void singCatSong() {
-        sing("We gave the cat to a little kid");
-        sing("But the cat came back");
-        sing("The very next day");
-        sing("Oh the cat came back");
-        sing("We thought he was a goner");
-        sing("But the cat came back, he just wouldn't go away");
-        sing("We sent the cat out on a boat");
+        singWith("We gave the cat to a little kid");
+        singWith("We sent the cat out on a boat");
+    }
+
+    private void singWith(String line) {
+        sing(line);
         sing("But the cat came back");
         sing("The very next day");
         sing("Oh the cat came back");
