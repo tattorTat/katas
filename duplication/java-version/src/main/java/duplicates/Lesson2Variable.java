4290f88d953f739e483818b6b70fc9577fcd351e
Muhammed Mutahr <mmutahr@ford.com>
2021-05-18 16:41:36 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- duplication/java-version/src/main/java/duplicates/Lesson2Variable.java
+++ duplication/java-version/src/main/java/duplicates/Lesson2Variable.java
@@ -3,15 +3,16 @@ package duplicates;
 public class Lesson2Variable extends Song {
     public void singBottlesOfBeer() {
         {
-            sing("100 bottles of beer on the wall");
-            sing("100 bottles of beer");
+            Integer num = 100;
+            sing(num + " bottles of beer on the wall");
+            sing(num + " bottles of beer");
             sing("Take one down, pass it around");
-            sing("99 bottles of beer on the wall");
+            sing((num - 1) + " bottles of beer on the wall");
         }
-
         {
-            sing("99 bottles of beer on the wall");
-            sing("99 bottles of beer");
+            Integer num = 99;
+            sing(num + " bottles of beer on the wall");
+            sing(num + " bottles of beer");
             sing("Take one down, pass it around");
             sing("98 bottles of beer on the wall");
         }
