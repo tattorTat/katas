841373a076379fa146825060730f6e410130ebc2
hharajl1 <hharajl1@ford.com>
2021-05-19 16:39:48 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- duplication/java-version/src/main/java/duplicates/Lesson21.java
+++ duplication/java-version/src/main/java/duplicates/Lesson21.java
@@ -3,29 +3,56 @@ package duplicates;
 public class Lesson21 extends Song {
     public void singSong(int style, String... names) {
         switch (style) {
-            case 1:
-                for (String name : names) {
-                    if (name.startsWith("L", 0)) {
-                        sing("Hip Hip Horray! For " + name);
-                    } else {
-                        sing("Hello " + name + ", it's nice to meet you.");
+            case 1: {
+
+                {
+                    for (String name : names) {
+                        foo(name, "L", 0, "Hip Hip Horray! For ");
                     }
                 }
-                break;
-            case 2:
-                for (String name : names) {
-                    if (name.startsWith("am", 1)) {
-                        sing("Say yeah! Say yo! Say " + name);
-                    } else {
-                        sing("Hello " + name + ", it's nice to meet you.");
+
+
+
+            }
+
+            break;
+            case 2: {
+
+
+                {
+                    for (String name : names) {
+                        foo(name, "am", 1, "Say yeah! Say yo! Say ");
                     }
                 }
-                break;
-            case 3:
+
+
+
+
+            }
+
+            break;
+            case 3: {
+
+
+
                 for (String name : names) {
                     sing("Hello " + name + ", it's nice to meet you.");
                 }
-                break;
+
+
+
+
+            }
+            break;
+        }
+    }
+
+    private void foo(String name, String am, int i, String s) {
+        if (name.startsWith(am, i)) {
+            String stringOne = s;
+            sing(stringOne + name);
+        } else {
+            sing("Hello " + name + ", it's nice to meet you.");
         }
     }
 }
