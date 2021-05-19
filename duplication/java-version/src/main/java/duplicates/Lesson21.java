37815d184a2768ca1247597bbf225d9adcb82fcc
hharajl1 <hharajl1@ford.com>
2021-05-19 17:13:18 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- duplication/java-version/src/main/java/duplicates/Lesson21.java
+++ duplication/java-version/src/main/java/duplicates/Lesson21.java
@@ -3,44 +3,22 @@ package duplicates;
 public class Lesson21 extends Song {
     public void singSong(int style, String... names) {
         switch (style) {
-            case 1: {
-
-                {
-                    foo(names, "L", 0, "Hip Hip Horray! For ");
-                }
-
-
-
-            }
-
+            case 1:
+                    foo(names, true, "L", 0, "Hip Hip Horray! For ");
             break;
-            case 2: {
-
-
-                {
-                    foo(names, "am", 1, "Say yeah! Say yo! Say ");
-                }
-            }
+            case 2:
+                    foo(names, true, "am", 1, "Say yeah! Say yo! Say ");
             break;
-            case 3: {
-                for (String name : names) {
-                    if (name.startsWith("am", 1)) {
-                        String name2 = name + ", it's nice to meet you.";
-                        String stringOne = "Hello ";
-                        sing(stringOne + name2);
-                    } else {
-                        sing("Hello " + name + ", it's nice to meet you.");
-                    }
-                }
-            }
+            case 3:
+                foo(names, false, "null", 1, "Say yeah! Say yo! Say ");
             break;
         }
     }
 
-    private void foo(String[] names, String am, int i, String s) {
+    private void foo(String[] names, boolean b, String s, int i, String s2) {
         for (String name : names) {
-            if (name.startsWith(am, i)) {
-                String stringOne = s;
+            if (b && name.startsWith(s, i)) {
+                String stringOne = s2;
                 sing(stringOne + name);
             } else {
                 sing("Hello " + name + ", it's nice to meet you.");
