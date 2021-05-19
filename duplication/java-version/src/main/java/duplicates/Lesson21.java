da9b9b497cf8ddde914b05c29816903274b1dffc
awaghmar <awaghmar@ford.com>
2021-05-19 17:01:05 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- duplication/java-version/src/main/java/duplicates/Lesson21.java
+++ duplication/java-version/src/main/java/duplicates/Lesson21.java
@@ -3,30 +3,24 @@ package duplicates;
 public class Lesson21 extends Song {
     public void singSong(int style, String... names) {
         switch (style) {
-            case 1: {
-                foo(names, "L", 0, "Hip Hip Horray! For ");
-            }
+            case 1:
+                singSongHelper(names, "L", 0, "Hip Hip Horray! For ");
                 break;
-            case 2: {
-                foo(names, "am", 1, "Say yeah! Say yo! Say ");
-            }
+            case 2:
+                singSongHelper(names, "am", 1, "Say yeah! Say yo! Say ");
                 break;
-            case 3: {
+            case 3:
                 for (String name : names) {
                     sing("Hello " + name + ", it's nice to meet you.");
                 }
-            }
                 break;
         }
     }
 
-    private void foo(String[] songNames, String startSongWith, int startPosition, String celebrateTag) {
+    private void singSongHelper(String[] songNames, String startSongWith, int startPosition, String celebrateTag) {
         for (String name : songNames) {
-            String nameStart = startSongWith;
-            int offset = startPosition;
-            if (name.startsWith(nameStart, offset)) {
-                String celebrate = celebrateTag;
-                sing(celebrate + name);
+            if (name.startsWith(startSongWith, startPosition)) {
+                sing(celebrateTag + name);
             } else {
                 sing("Hello " + name + ", it's nice to meet you.");
             }
