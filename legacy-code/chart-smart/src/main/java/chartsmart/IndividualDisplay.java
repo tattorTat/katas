d5dbcb09c812194a47a1f31d2e34e885145e2e6a
Sarah Schroeder <sschro31@ford.com>
2021-06-02 16:23:19 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- legacy-code/chart-smart/src/main/java/chartsmart/IndividualDisplay.java
+++ legacy-code/chart-smart/src/main/java/chartsmart/IndividualDisplay.java
@@ -67,39 +67,13 @@ public class IndividualDisplay extends JPanel {
 
     private void DrawChart(Graphics graphics) {
         displaySetColorAndText(graphics);
-        extracted(graphics);
+        extractedv1(graphics);
     }
 
-    private void extracted(Graphics graphics) {
+    private void extractedv1(Graphics graphics) {
         Font font;
         if (chartType == BAR_CHART) {
-            String[] charts = getBarChartData();
-            if (displayStyle.equals(SINGLE_MODE)) {
-                int bottomY = 500;
-                graphics.setColor(Color.CYAN);
-                graphics.fillRect(112, bottomY - 200, 75, 200);
-                graphics.fillRect(187, bottomY - 400, 75, 400);
-                graphics.fillRect(262, bottomY - 300, 75, 300);
-                graphics.fillRect(337, bottomY - 250, 75, 250);
-                graphics.fillRect(412, bottomY - 340, 75, 340);
-                font = new Font("Arial Black", Font.BOLD, 55);
-                graphics.setColor(Color.BLACK);
-                graphics.setFont(font);
-                graphics.drawString(charts[0], 130, 400);
-            } else {
-                font = new Font("Arial Black", Font.BOLD, 25);
-                graphics.setColor(Color.CYAN);
-                int bottomY = 300;
-                graphics.fillRect(100, bottomY - 100, 40, 100);
-                graphics.fillRect(140, bottomY - 200, 40, 200);
-                graphics.fillRect(180, bottomY - 150, 40, 150);
-                graphics.fillRect(220, bottomY - 125, 40, 125);
-                graphics.fillRect(260, bottomY - 170, 40, 170);
-                graphics.setColor(Color.RED);
-                graphics.setFont(font);
-                graphics.drawString(charts[0], 130, 250);
-                graphics.drawString(charts[1], 130, 270);
-            }
+            extracted(graphics);
         } else {
             String[] pieChartData = new String[0];
             List<String> getPositionalData = new ArrayList<>();
@@ -116,6 +90,37 @@ public class IndividualDisplay extends JPanel {
                 graphics.drawString(pieChartData[0], 145, 205);
                 graphics.drawString(pieChartData[1], 170, 235);
             }
+        }
+    }
+
+    private void extracted(Graphics graphics) {
+        Font font;
+        String[] charts = getBarChartData();
+        if (displayStyle.equals(SINGLE_MODE)) {
+            int bottomY = 500;
+            graphics.setColor(Color.CYAN);
+            graphics.fillRect(112, bottomY - 200, 75, 200);
+            graphics.fillRect(187, bottomY - 400, 75, 400);
+            graphics.fillRect(262, bottomY - 300, 75, 300);
+            graphics.fillRect(337, bottomY - 250, 75, 250);
+            graphics.fillRect(412, bottomY - 340, 75, 340);
+            font = new Font("Arial Black", Font.BOLD, 55);
+            graphics.setColor(Color.BLACK);
+            graphics.setFont(font);
+            graphics.drawString(charts[0], 130, 400);
+        } else {
+            font = new Font("Arial Black", Font.BOLD, 25);
+            graphics.setColor(Color.CYAN);
+            int bottomY = 300;
+            graphics.fillRect(100, bottomY - 100, 40, 100);
+            graphics.fillRect(140, bottomY - 200, 40, 200);
+            graphics.fillRect(180, bottomY - 150, 40, 150);
+            graphics.fillRect(220, bottomY - 125, 40, 125);
+            graphics.fillRect(260, bottomY - 170, 40, 170);
+            graphics.setColor(Color.RED);
+            graphics.setFont(font);
+            graphics.drawString(charts[0], 130, 250);
+            graphics.drawString(charts[1], 130, 270);
         }
     }
 
