f7f7e4cd4a8eb4b7bbad881c8134c4842351c602
awaghmar <awaghmar@ford.com>
2021-06-07 16:47:59 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- legacy-code/chart-smart/src/main/java/chartsmart/PieChart.java
+++ legacy-code/chart-smart/src/main/java/chartsmart/PieChart.java
@@ -20,10 +20,14 @@ public class PieChart implements Serializable {
     }
 
     void drawPieChart(Graphics graphics, String displayStyle, String singleMode) {
-        Font font;
         String[] pieChartData = new String[0];
         List<String> getPositionalData = new ArrayList<>();
         String[] pieChartData1 = pieChartData;
+        getPiChartData(graphics, displayStyle, singleMode, getPositionalData, pieChartData1);
+    }
+
+    private void getPiChartData(Graphics graphics, String displayStyle, String singleMode, List<String> getPositionalData, String[] pieChartData1) {
+        Font font;
         if (displayStyle.equals(singleMode)) {
             getPositionalData.add("Pie Chart");
         } else {
@@ -31,7 +35,8 @@ public class PieChart implements Serializable {
             pieChartData1[1] = "Small";
             pieChartData1[0] = "Pie Chart";
         }
-        pieChartData = pieChartData1;
+
+        String[] pieChartData = pieChartData1;
         if (displayStyle.equals(singleMode)) {
             font = new Font("Bookman Old Style", Font.BOLD, 55);
             graphics.setColor(Color.WHITE);
